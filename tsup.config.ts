6a16ae10b26c8395693e64bb1c42bf01720f535b
KimSohyun <735039922@qq.com>
2025-04-24 17:32:32 +0800

    update

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'tsup';
-import fs from 'fs';
-import path from 'path';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  splitting: false,
-  sourcemap: true,
+  sourcemap: false,
   clean: true,
   format: ['esm', 'cjs'],
   dts: true,
